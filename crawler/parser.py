529770db5d42df5e81f2b10284c926ffd33d4334
joestephens <joe.stephens@live.co.uk>
2016-08-08 23:02:39 +0100

    url parsing

--- crawler/parser.py
+++ crawler/parser.py
@@ -1,31 +1,51 @@
 from html.parser import HTMLParser
+from urllib.parse import unquote
 import requests
+import re
 
 class Parser(HTMLParser):
-	
-	def __init__(self):
+
+	def __init__(self, webpage):
 		HTMLParser.__init__(self)
+		self.webpage = webpage
 		self.data = { 'webpage_urls': [] }
 		self.last_tag = None
-		self.ignore_tags = ['script', 'style', 'link', 'html', 'head', 'button', 'img', 'input', 'label']
-		
+		self.allowed_tags = ['meta', 'title', 'h1', 'h2', 'h3', 'p']
+
 	def handle_starttag(self, tag, attrs):
 		if tag == 'a':
-			self.data['webpage_urls'].append(attrs[0][1])
-			#print(attrs[0][1])
-		
-		if tag not in self.ignore_tags:	
+			url = self.__parse_url(attrs[0][1])
+
+			if url:
+				self.__add_link(url)
+
+		if tag in self.allowed_tags:
 			self.data[tag] = None
-			
+
 		self.last_tag = tag
-		#print(self.data)
 
 	def handle_data(self, data):
-		if self.last_tag not in self.ignore_tags:
-			self.data[self.last_tag] = data
-			#print(self.data)
+		if not data.strip():
+			return None
 
-parser = Parser()
-r = requests.get("http://www.bbc.co.uk/news/election-us-2016-36990724")
-parser.feed(r.text)
-print(parser.data)     
+		if self.last_tag in self.allowed_tags:
+			self.data[self.last_tag] = unquote(data)
+
+	def __parse_url(self, url):
+		if not (url.startswith('http') or url.startswith('/')):
+			return None
+
+		return self.__full_url(url)
+
+	def __add_link(self, url):
+		self.data['webpage_urls'].append(url)
+
+	def __full_url(self, url):
+		if not (url.startswith('http')):
+			return self.__get_partial() + url
+
+		return url
+
+	def __get_partial(self):
+		url = re.search('(http[s]?|ftp):\/?\/?([^:\/\s]+)(\/\w+)', self.webpage)
+		return url.group(0)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,14 +2,14 @@ import unittest
 from crawler.parser import Parser
 
 class TestParser(unittest.TestCase):
-	
+
 	def setUp(self):
-		self.parser = Parser()
+		self.parser = Parser("http://google.co.uk")
 		self.urls = ["http://google.co.uk", "http://news.bbc.co.uk"]
-		
+
 	def tearDown(self):
 		pass
-		
+
 	def test_get_start_tag(self):
 		body = '<title>'
 		self.parser.feed(body)
@@ -19,8 +19,13 @@ class TestParser(unittest.TestCase):
 		body = '<title>Hey'
 		self.parser.feed(body)
 		self.assertEqual(self.parser.data['title'], 'Hey')
-		
+
 	def test_get_links(self):
 		body = "<h1>Hey</h1><a href='%s'>Link 1</a><a href='%s'>Link 2</a>" % (self.urls[0], self.urls[1])
 		self.parser.feed(body)
 		self.assertEqual(self.parser.data['webpage_urls'], self.urls)
+
+	def test_ignores_tags(self):
+		body = "<script>some js</script><style>some css</style><h1>hey</h1>"
+		self.parser.feed(body)
+		self.assertEqual(self.parser.data, { 'h1': 'hey', 'webpage_urls': [] })
